13a647a3c2cbeede7f734800fe8b46ce517c10c9
onumengine <onumdev@gmail.com>
2021-03-18 20:19:44 +0100

    added a listener to the nav open button

---
+++ script.js
@@ -0,0 +1,8 @@
+window.onload = main;
+
+function main() {
+    var navOpenButton = document.getElementById('menuButton');
+    navOpenButton.addEventListener('click', openNavDrawer);
+}
+
+function openNavDrawer() {}
