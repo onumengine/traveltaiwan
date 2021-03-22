cbfb3e0dd069c2cfe285c3e744352f947d89190f
onumengine <onumdev@gmail.com>
2021-03-22 11:45:21 +0100

    removed the  debug statements from the javascript

--- script.js
+++ script.js
@@ -9,17 +9,13 @@ function main() {
 }
 
 function openNavDrawer() {
-    console.log('trying to open nav drawer');
     var navDrawer = document.querySelector('.appbarActions');
-    console.log(navDrawer);
     navDrawer.style.display = "block";
     navDrawer.style.width = '90%';
 }
 
 function closeNavDrawer() {
-    console.log('trying to close nav');
     var navDrawer = document.querySelector('.appbarActions');
-    console.log(navDrawer);
     navDrawer.style.width = '0';
     navDrawer.style.display = "none";
 }
